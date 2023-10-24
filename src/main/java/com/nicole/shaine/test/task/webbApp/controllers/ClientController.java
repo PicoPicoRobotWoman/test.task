26b65be8dd6b549d80232ce809f4ecc7e85bb176
PicoPicoRobotWoman <i.ivanov@qvant.ru>
2023-10-24 15:50:11 +0300

    feat: добавить минимальную реализацию ClientController

---
+++ src/main/java/com/nicole/shaine/test/task/webbApp/controllers/ClientController.java
@@ -0,0 +1,12 @@
+package com.nicole.shaine.test.task.webbApp.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/client")
+public class ClientController {
+
+
+
+}
