35fe97f3015c626daa08ec0d43eed4fea4da8604
PicoPicoRobotWoman <i.ivanov@qvant.ru>
2023-10-24 16:24:52 +0300

    feat: добавить эндопоинт для получения всех клиентов

--- src/main/java/com/nicole/shaine/test/task/webbApp/controllers/ClientController.java
+++ src/main/java/com/nicole/shaine/test/task/webbApp/controllers/ClientController.java
@@ -1,12 +1,31 @@
 package com.nicole.shaine.test.task.webbApp.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.nicole.shaine.test.task.models.entitys.Client;
+import com.nicole.shaine.test.task.service.abs.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/client")
 public class ClientController {
 
+    private final ClientService clientService;
 
+    @Autowired
+    public ClientController(ClientService clientService) {
+        this.clientService = clientService;
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<Client> getClients() {
+
+        return clientService.getAll();
+
+    }
 
 }
