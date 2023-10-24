4f6c9a3b3950b8129a4d2816f5536839df1f2397
PicoPicoRobotWoman <i.ivanov@qvant.ru>
2023-10-24 15:47:40 +0300

    feat: добавить минимальную реализацию ClientServiceImpl

---
+++ src/main/java/com/nicole/shaine/test/task/service/abs/ClientService.java
@@ -0,0 +1,6 @@
+package com.nicole.shaine.test.task.service.abs;
+
+import com.nicole.shaine.test.task.models.entitys.Client;
+
+public interface ClientService extends AbstractService<Long, Client> {
+}
---
+++ src/main/java/com/nicole/shaine/test/task/service/impl/ClientServiceImpl.java
@@ -0,0 +1,20 @@
+package com.nicole.shaine.test.task.service.impl;
+
+import com.nicole.shaine.test.task.dao.abs.AbstractDao;
+import com.nicole.shaine.test.task.dao.abs.ClientDao;
+import com.nicole.shaine.test.task.models.entitys.Client;
+import com.nicole.shaine.test.task.service.abs.AbstractServiceImpl;
+import com.nicole.shaine.test.task.service.abs.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClientServiceImpl extends AbstractServiceImpl<Long, Client> implements ClientService {
+
+    @Autowired
+    public ClientServiceImpl(ClientDao clientDao) {
+        super(clientDao);
+    }
+
+
+}
