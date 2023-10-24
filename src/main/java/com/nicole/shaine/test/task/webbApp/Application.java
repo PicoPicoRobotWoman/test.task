371108e244ef0a58c92259801f17d5020b304849
PicoPicoRobotWoman <i.ivanov@qvant.ru>
2023-10-24 14:25:16 +0300

    feat: настроить main класс для jpa

--- src/main/java/com/nicole/shaine/test/task/webbApp/Application.java
+++ src/main/java/com/nicole/shaine/test/task/webbApp/Application.java
@@ -1,11 +1,15 @@
-package com.nicole.shaine.test.task;
+package com.nicole.shaine.test.task.webbApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @ComponentScan({"com.nicole.shaine.test.task"})
+@EntityScan(value = "com.nicole.shaine.test.task")
+@EnableTransactionManagement
 public class Application {
 
 	public static void main(String[] args) {
